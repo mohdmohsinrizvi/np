437754d1ccac560377267333c1caac755a44d59b
Mohammed mohsin <70643623+mohdmohsinrizvi@users.noreply.github.com>
2025-01-25 13:07:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { motion } from "framer-motion"
+/*import { motion } from "framer-motion"*/
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import About from "./components/About"
